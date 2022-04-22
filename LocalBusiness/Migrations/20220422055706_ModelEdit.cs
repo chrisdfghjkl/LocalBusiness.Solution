483da104089f8270a9b1bce8fc938b593d694bcf
chrisdfghjkl <cdepastene@gmail.com>
2022-04-21 22:57:34 -0700

    add new migration

---
+++ LocalBusiness/Migrations/20220422055706_ModelEdit.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalBusiness.Migrations
+{
+    public partial class ModelEdit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Businesses");
+
+            migrationBuilder.DropColumn(
+                name: "Review",
+                table: "Businesses");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Businesses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Review",
+                table: "Businesses",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+    }
+}
