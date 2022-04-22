2a99c9d40c55b95607ec8f9545eb5471c88e8969
chrisdfghjkl <cdepastene@gmail.com>
2022-04-22 15:19:59 -0700

    Add update and delete routes

--- LocalBusiness/Controllers/LocalBusinessesController.cs
+++ LocalBusiness/Controllers/LocalBusinessesController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LocalBusiness.Models;
@@ -36,7 +39,37 @@ namespace LocalBusiness.Controllers
       }
 
       return business;
-}
+    }
+
+    // PUT: api/Businesses/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Business business)
+    {
+      if (id != business.BusinessId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(business).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!BusinessExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
 
     // POST api/businesses
     [HttpPost]
@@ -46,6 +79,27 @@ namespace LocalBusiness.Controllers
       await _db.SaveChangesAsync();
 
       return CreatedAtAction(nameof(GetBusiness), new { id = business.BusinessId }, business);
+    }
+
+    // DELETE: api/Businesses/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBusiness(int id)
+    {
+      var business = await _db.Businesses.FindAsync(id);
+      if (business == null)
+      {
+        return NotFound();
+      }
+
+      _db.Businesses.Remove(business);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool BusinessExists(int id)
+    {
+      return _db.Businesses.Any(e => e.BusinessId == id);
     }
   }
 }
