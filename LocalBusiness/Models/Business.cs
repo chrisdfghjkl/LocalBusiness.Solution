d4ed1a94fef5d3bf997ce280b781e5b56445a0fd
chrisdfghjkl <cdepastene@gmail.com>
2022-04-22 20:18:25 -0700

    add to business class

--- LocalBusiness/Models/Business.cs
+++ LocalBusiness/Models/Business.cs
@@ -8,6 +8,7 @@ namespace LocalBusiness.Models
         public string Description { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
+        public string City { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
     }
