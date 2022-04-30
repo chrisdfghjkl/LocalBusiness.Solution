4bfdbd6fb23bf675ba7a5a3659e1bc798cab668f
chrisdfghjkl <cdepastene@gmail.com>
2022-04-29 19:25:14 -0700

    add swagger implementation

--- LocalBusiness/Startup.cs
+++ LocalBusiness/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using LocalBusiness.Models;
 
@@ -24,6 +25,14 @@ namespace LocalBusiness
             services.AddDbContext<LocalBusinessContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Version = "v1",
+                    Title = "LocalBusiness",  
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +41,12 @@ namespace LocalBusiness
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
             }
 
             // app.UseHttpsRedirection();
